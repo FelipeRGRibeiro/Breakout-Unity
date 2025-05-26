b34528172de0b4b27dbf5cf9753a01c7a524ae16
Felipe Ribeiro <feliper.g.ribeiro@gmail.com>
2025-05-26 20:29:28 -0300

    Adição da cena do menu inicial e script

---
+++ Assets/Scripts/MenuInicial.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor.SceneManagement;
+
+public class MenuInicial : MonoBehaviour
+{
+    public Button newGame;
+    public Button exit;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        // Verifica se o botão foi atribuído no Inspector
+        if (newGame != null)
+        {
+            // Adiciona um listener ao botão
+            newGame.onClick.AddListener(mainScene);
+        }
+        if (exit != null)
+        {
+            // Adiciona um listener ao botão
+            exit.onClick.AddListener(quit);
+        }
+    }
+    void mainScene()
+    {
+        EditorSceneManager.LoadScene("MainGame");
+        // Coloque aqui o que deseja que aconteça quando o botão for clicado
+    }
+    void quit()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        // Para parar o jogo dentro do editor
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
