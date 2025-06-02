d4f96e593530b6c40e3945bafa8922c633ebf107
Felipe Ribeiro <feliper.g.ribeiro@gmail.com>
2025-06-02 20:31:40 -0300

    Elemento de placar ao morrer.

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -1,14 +1,33 @@
+using UnityEditor.SceneManagement;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class Restart : MonoBehaviour
 {
+    public Button ConfirmButton;
+    public GameObject ScoreBoard;
+    public TextMeshProUGUI finalScore;
+    private void Start()
+    {
+        ScoreBoard.SetActive(false);
+        if (ConfirmButton != null)
+        {
+            // Adiciona um listener ao botão
+            ConfirmButton.onClick.AddListener(Renew);
+        }
+    }
     private void OnTriggerExit2D(Collider2D collision)
     {
         Debug.Log("Morreu");
-        Application.Quit();
+        finalScore.text += Player.score.ToString();
+        ScoreBoard.SetActive(true);
+    }
+    private void Renew()
+    {
+        EditorSceneManager.LoadScene("Menu");
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
-#endif   
+#endif  
     }
 }
